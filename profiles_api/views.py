57acace1aa5a09bb1acdc0d8f0f87bcf8639aeb1
govindsaj <govindsajfb@gmail.com>
2020-08-14 10:39:23 +0530

    Adeed user profile feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,8 +7,10 @@ from profiles_api import models
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from profiles_api import permissions
-
+from rest_framework.permissions import IsAuthenticated
 
 class HelloApiView(APIView):
     """ Test API View """
@@ -109,3 +111,24 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    """Handle creating user authentication tokens """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """ Handles creating,reading and updating profile feed items"""
+
+    serializer_class=serializers.ProfileFeedItemSerializer
+    authentication_classes= (TokenAuthentication,)
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated,
+    )
+
+    def perform_create(self,serializer):
+        """Sets the user profile to the loggen in user"""
+        serializer.save(user_profile = self.request.user)
